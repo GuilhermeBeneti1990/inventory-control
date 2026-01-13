d3c819f916a1a342041825356311b9a401c88a83
Guilherme Beneti <guilhermebeneti@icloud.com>
2026-01-13 17:37:44 -0300

    fix: fixing authentication middleware

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -18,7 +18,7 @@ export function isAuthenticated(
   try {
     const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
     request.user_id = sub;
-    return next;
+    return next();
   } catch (error) {
     return response.status(401).end();
   }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { DetailUserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
 router.get("/health", (request: Request, response: Response) => {
@@ -10,5 +12,6 @@ router.get("/health", (request: Request, response: Response) => {
 //User Routes
 router.post("/users", new CreateUserController().handle);
 router.post("/users/auth", new AuthUserController().handle);
+router.get("/users/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
